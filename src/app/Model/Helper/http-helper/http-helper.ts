5fe69a248c5dd6d64f236ecc3ff2b036720810e8
Malloc72P <dase1102@gmail.com>
2020-01-16 23:13:20 +0900

    변경내용 : 배포용 브랜치
    1) URL주소를 팀원 : 나승철의 NAS주소로 수정함

--- src/app/Model/Helper/http-helper/http-helper.ts
+++ src/app/Model/Helper/http-helper/http-helper.ts
@@ -16,10 +16,10 @@ export enum ApiRequestTypeEnum {
 
 export class HttpHelper {
   //TODO 이런거 json으로 뽑을 수 있으면 뽑아야 함.
-  private static readonly ngDomainName        =   "http://localhost";
-  private static readonly ngPort              =   ":4200";
-  private static readonly apiServerDomainName =   "http://localhost";
-  private static readonly apiServerPort       =   ":5200";
+  private static readonly ngDomainName        =   "http://skynet765.iptime.org";
+  private static readonly ngPort              =   ":44172";
+  private static readonly apiServerDomainName =   "http://skynet765.iptime.org";
+  private static readonly apiServerPort       =   ":44174";
   // private static readonly apiServerPort       =   ":5858";
   private static readonly contentType         =   'application/json; charset=utf-8';
   private static readonly tokenType           =   'bearer ';
