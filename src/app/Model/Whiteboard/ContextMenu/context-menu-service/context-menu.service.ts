f534bfe20b7c0600b9464dfd58a4a8286fc9b55a
malloc72p <dase1102@gmail.com>
2020-04-13 00:05:26 +0900

    변경내용 : openMenu 로직 재구축
    1) 페이퍼 프로젝트가 여러개가 되면서 기존 openMenu로직에 문제가 생겨서 새로 로직을 짜고 구축함
    2) 원노트의 그리기도구 성능개선 페이지에 해당 로직 플로우차트 그려놓았음.

--- src/app/Model/Whiteboard/ContextMenu/context-menu-service/context-menu.service.ts
+++ src/app/Model/Whiteboard/ContextMenu/context-menu-service/context-menu.service.ts
@@ -65,38 +65,85 @@ export class ContextMenuService {
     let convertedPoint = this.positionCalcService.advConvertNgToPaper(point);
 
     let hitItem = this.layerService.getHittedItem(convertedPoint);
-    let isGsg = this.layerService.isHitGSG(convertedPoint);
-
     if(hitItem instanceof WhiteboardItem){
       this.item = hitItem;
-      console.log("ContextMenuService >> openMenu >> hitItem : ",hitItem);
-      if(!isGsg){
-        this.layerService.globalSelectedGroup.extractAllFromSelection();
+    }
+
+    console.log("ContextMenuService >> openMenu >> 진입함");
+
+    //1. GSG가 비어있는지 검사
+    if(this.layerService.globalSelectedGroup.getNumberOfChild() === 0){
+      //비어있다면
+      //히트된 개체가 있으면 GSG에 넣는다
+      //그리고 해당 개체에 맞는 메뉴모드로 동작한다
+      if(hitItem){
+        this.layerService.globalSelectedGroup.insertOneIntoSelection(hitItem);
+        this.setMenuByInstance(hitItem);
+      }else{
+        //히트되지 않았다면, 공백영역을 클릭하거나 터치한것. 디폴트 메뉴모드로 동작
+        this.setContextMenuToDefault();
+      }
+    }else{
+      //안비었다면
+      //우선 히트되었는지 검사
+      if(hitItem){
+        //히트된 아이템이 선택된 상태인지 검사(이는 GSG에 포함되었는지랑 같은 의미임.)
+        if(hitItem.isSelected){
+          //이미 아이템을 선택중이며, 선택중인 개체에 대고 멘뉴를 열려고 한 경우
+          //GSG개체수가 1개인지 검사
+          if(this.layerService.globalSelectedGroup.getNumberOfChild() === 1){
+            //1개라면, 해당 개체에 맞는 메뉴모드로 동작
+            this.setMenuByInstance(hitItem);
+          }else{
+            //1개보다 많다면 그룹용 메뉴모드로 동작
+            this.setContextMenuToGroup();
+          }
+        }else if(this.layerService.isHitGSG(convertedPoint)){
+          //개체는 히트했고 GSG에 포함된 개체는 아닌데 해당 영역이 GSG의 영역인 경우
+          //그룹용 메뉴모드로 동작
+          this.setContextMenuToGroup();
+        }else{
+          //개체는 히트했으나, GSG에 포함된 개체도 아니고, GSG영역도 아닌 경우
+          //GSG를 비우고 히트된 아이템을 GSG에 넣는다.
+          this.layerService.globalSelectedGroup.extractAllFromSelection();
+          this.layerService.globalSelectedGroup.insertOneIntoSelection(hitItem);
+          //그런 다음, 해당 개체유형에 맞는 메뉴모드로 동작한다.
+          this.setMenuByInstance(hitItem);
+        }
+      }else{
+        //히트된 아이템이 없는 경우에는...
+        //메뉴오픈 이벤트가 발생한 좌표가 GSG인지 검사한다.
+        if(this.layerService.isHitGSG(convertedPoint)){
+          //만약 GSG영역에서 이벤트가 발생한 경우라면, 그룹용 메뉴모드로 동작한다
+          this.setContextMenuToGroup();
+        }else{
+          //GSG영역에서 발생한 이벤트가 아니라면
+          // 이는 GSG밖을 클릭하거나 터치한 경우이다. 따라서 공백영역을 클릭하거나 터치한것임.
+          //GSG를 다 비우고 디폴트 메뉴모드로 동작한다
+          this.layerService.globalSelectedGroup.extractAllFromSelection();
+          this.setContextMenuToDefault();
+        }
       }
     }
 
-    if(isGsg){//gsg를 선택한 경우
-      this.setContextMenuToGroup();
-    }else{//gsg가 아닌 개체를 선택한 경우
-      if(hitItem instanceof EditableStroke) {
-        this.setContextMenuToStroke();
-      } else if(hitItem instanceof EditableShape){
-        this.setContextMenuToShape();
-      } else if(hitItem instanceof EditableRaster){
-        this.setContextMenuToRaster();
-      } else if(hitItem instanceof ItemGroup){
-        this.setContextMenuToGroup();
-      } else if(hitItem instanceof EditableLink){
-        this.setContextMenuToEditableLink();
-      } else {
-        this.setContextMenuToDefault();
-      }
-      if(hitItem){
-        this.layerService.globalSelectedGroup.insertOneIntoSelection(hitItem);
-      }
-    }
     this.setPosition(point);
     this.contextMenuTrigger.openMenu();
+  }
+
+  setMenuByInstance(hitItem){
+    if(hitItem instanceof EditableStroke) {
+      this.setContextMenuToStroke();
+    } else if(hitItem instanceof EditableShape){
+      this.setContextMenuToShape();
+    } else if(hitItem instanceof EditableRaster){
+      this.setContextMenuToRaster();
+    } else if(hitItem instanceof ItemGroup){
+      this.setContextMenuToGroup();
+    } else if(hitItem instanceof EditableLink){
+      this.setContextMenuToEditableLink();
+    } else {
+      this.setContextMenuToDefault();
+    }
   }
 
   private initPoint(event: MouseEvent | TouchEvent): Point {
